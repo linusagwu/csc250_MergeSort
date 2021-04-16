b77762fd014286a4b77f5d8f636151ddc6aa2517
linusagwu <78839892+linusagwu@users.noreply.github.com>
2021-04-16 14:42:14 -0400

    HW #17

--- app/src/main/java/com/example/csc250_mergesort/MainActivity.java
+++ app/src/main/java/com/example/csc250_mergesort/MainActivity.java
@@ -4,33 +4,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-    private EditText inputET;
-    private TextView answerTV;
+    private ListView listOfNumberLV;
+    private ArrayList<String> theListOfNumbersAsStrings;
+    private ArrayAdapter<String> theListOfNumbersAdapter;
 
+    private ListView theCallsToMergeSortLV;
+    private ArrayList<String> theListOfMergeSortCalls;
+    private ArrayAdapter<String> theCallsToMergeSortAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.inputET = this.findViewById(R.id.inputET);
-        this.answerTV = this.findViewById(R.id.answerTV);
+        this.listOfNumberLV = this.findViewById(R.id.listOfNumberLV);
+
+        //this guy will show the parts of the arrayList we are working on in MergeSort
+        this.theListOfMergeSortCalls = new ArrayList<String>();
+        this.theCallsToMergeSortAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                this.theListOfMergeSortCalls);
+        this.theCallsToMergeSortLV = this.findViewById(R.id.theCallsToMergeSortLV);
+        this.theCallsToMergeSortLV.setAdapter(this.theCallsToMergeSortAdapter);
+
+
+        this.theListOfNumbersAsStrings = new ArrayList<String>();
+        this.theListOfNumbersAsStrings.add("5");
+        this.theListOfNumbersAsStrings.add("2");
+        this.theListOfNumbersAsStrings.add("8");
+        this.theListOfNumbersAsStrings.add("3");
+        this.theListOfNumbersAsStrings.add("13");
+        this.theListOfNumbersAsStrings.add("5");
+        this.theListOfNumbersAsStrings.add("5");
+        this.theListOfNumbersAsStrings.add("8");
+        this.theListOfNumbersAsStrings.add("1");
+
+       // this.theListOfNumbersAdapter = new ArrayAdapter<String>(this,
+           //     android.R.layout.simple_list_item_1,
+             //   this.theListOfNumbersAsStrings);
+        this.theListOfNumbersAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                 this.theListOfNumbersAsStrings);
+
+        this.listOfNumberLV.setAdapter(theListOfNumbersAdapter);
+
     }
-    public void onFactorialButtonClicked(View v)
+    private String buildStringFromPartOfList(ArrayList<String> theList, int begin, int end)
     {
-        String currValue = this.inputET.getText().toString();
-        int n = Integer.parseInt(currValue);
-        this.answerTV.setText(String.valueOf(fact(n)));
+        String s = "";
+        for(int i = begin; i <= end; i++)
+        {
+            s = s + theList.get(i) + " ";
+        }
+        return s;
     }
-    private long fact(long n)
+
+    private void mergeSort(ArrayList<String> theList, int begin, int end)
     {
-        if (n <= 1)
-            return 1;
-        else
-            return n * fact(n - 1);
+        //ask if this list is trivially sorted
+        //I only want to do anything inside mergeSort if it is NOT trivially sorted
+        String currentPartOfArray = this.buildStringFromPartOfList(theList, begin, end);
+        this.theListOfMergeSortCalls.add(currentPartOfArray);
+        this.theCallsToMergeSortAdapter.notifyDataSetChanged();
+
+        if(begin != end)
+        {
+            //I do NOT have a 1-list, so I need to divide my list in half and
+            //call MergeSort on the right and the left
+            //HW: update the theListOfMergeSortCalls to show all of the individual calls we
+            //make to MergeSort
+            int begin1 = begin;
+            int end2 = end;
+        }
+    }
+    public void onMergeSortButtonClicked(View v)
+    {
+        this.mergeSort(this.theListOfNumbersAsStrings,
+                0, this.theListOfNumbersAsStrings.size()-1);
     }
 }
